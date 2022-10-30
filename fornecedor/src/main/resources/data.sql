ee184028bc267310fddcf2e3c1a712e9c5b9bd7a
Alexandre Oliveira <alexmdo@gmail.com>
2022-10-30 19:48:47 -0300

    Adaptando config para o cenario do projeto do curso anterior

---
+++ fornecedor/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into info_fornecedor (nome, estado, endereco) values ('Maria', 'GO', 'Endereco da Marai');
+
+insert into produto (nome, estado, descricao, preco) values ('rosas', 'GO', 'rosas', 1.99);
+insert into produto (nome, estado, descricao, preco) values ('orquidea', 'GO', 'orquidea', 25.00);
+insert into produto (nome, estado, descricao, preco) values ('girassol', 'GO', 'girassol', 5.00);
