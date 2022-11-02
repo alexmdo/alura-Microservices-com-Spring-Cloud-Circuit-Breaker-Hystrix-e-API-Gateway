ab0e3cb85d81a2c260be7134ac3f5b15fb9449ba
Alexandre Oliveira <alexmdo@gmail.com>
2022-11-02 18:23:43 -0300

    Feign interceptor

--- loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
+++ loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -7,7 +9,10 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -15,7 +20,23 @@ import org.springframework.web.client.RestTemplate;
 @EnableCircuitBreaker
 @EnableResourceServer
 public class LojaApplication {
-	
+
+	@Bean
+	public RequestInterceptor requestInterceptor() {
+		return new RequestInterceptor() {
+			@Override
+			public void apply(RequestTemplate requestTemplate) {
+				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+				if (authentication == null) {
+					return;
+				}
+
+				OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+				requestTemplate.header("Authorization", "Bearer " + details.getTokenValue());
+			}
+		};
+	}
+
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
